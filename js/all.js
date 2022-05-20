d1961307b96cd91b790321c5ccf3e900529e5b0a
Linailnox <3419588389@qq.com>
2022-05-20 13:00:40 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
-//这段js要放在页面最下方  
+/*/这段js要放在页面最下方  
 var h = window.innerHeight,w=window.innerWidth;  
 //禁用右键 （防止右键查看源代码）  
-window.οncοntextmenu=function(){return false;}  
+window.oncontextmenu=function(){return false;}  
 //在本网页的任何键盘敲击事件都是无效操作 （防止F12和shift+ctrl+i调起开发者工具）  
 window.onkeydown = window.onkeyup = window.onkeypress = function () {  
     window.event.returnValue = false;  
@@ -13,7 +13,7 @@ window.onresize = function () {
         window.close();  
         window.location = "about:blank";  
     }  
-}  
+}  */
 /*好吧，你的开发者工具是单独的窗口显示，不会改变原来网页的高度和宽度， 
 但是你只要修改页面元素我就重新加载一次数据,让你无法修改页面元素（不支持IE9以下浏览器）*/  
 if(window.addEventListener){  
