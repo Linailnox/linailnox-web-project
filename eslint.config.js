2cf27b0c729a89eee5789b53a5f2a600bda9744b
Linailnox <3419588389@qq.com>
2024-11-14 17:30:23 +0800

    chore: Remove lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import pluginPrettierRecommendedConfigs from 'eslint-plugin-prettier/recommended'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+	  files: ["**/*.{js,mjs,cjs,ts,vue}"]
+  },
+  {
+	  languageOptions: {
+		  globals: {
+			  ...globals.browser,
+			  ...globals.node,
+			  // esnext?
+			  ...globals.es2025
+		  }
+	  }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+	pluginPrettierRecommendedConfigs,
+  {
+	  files: ["**/*.vue"],
+	  languageOptions: {
+		  parserOptions: {
+			  parser: tseslint.parser
+		  }
+	  }
+	  },
+];
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+export default {
+	trailingComma: "es5",
+	tabWidth: 4,
+	semi: false,
+	printWidth: 100,
+	endOfLine: "crlf",
+	useTabs: true,
+	singleQuote: true,
+};
