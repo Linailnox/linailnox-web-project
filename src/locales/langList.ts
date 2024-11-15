b68a176ec5b39ece8b36c193122200686c72ff84
Linailnox <3419588389@qq.com>
2024-11-15 23:40:59 +0800

    chore: Better i18n support

---
+++ src/locales/langList.ts
@@ -0,0 +1,21 @@
+const langList = [
+	{
+		// vue-i18n 中的语言字段 key
+		category: "zh",
+		// 当前语言名 key
+		lang: "简体中文",
+		// Element Plus 中的语言字段 key
+		el_lang: "zh-CN",
+	},
+	{
+		category: "en",
+		lang: "English",
+		el_lang: "en",
+	},
+]
+const defaultLang = {
+	category: "zh",
+	lang: "简体中文",
+	el_lang: "zh-CN",
+}
+export { langList, defaultLang }
